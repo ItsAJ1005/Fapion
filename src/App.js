18e32f689e460f17f635e671d95967a04c26ce4c
ItsAJ1005 <ajharshvardhan123@gmail.com>
2024-08-27 22:31:42 +0530

    Add - sign in component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import './index.scss'
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
+import SignIn from './components/signIn/signInComponent';
 
 function App() {
     return (
       <Routes>
         <Route path='/' element={<Navigation/>} >
             <Route index element={<Home/>}/>
+            <Route path='/signin' element={<SignIn/>}/>
         </Route>
       </Routes>
     )
